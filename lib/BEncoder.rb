d0061cc25a6517e2a55d69bfb2417f464223c439
Viksit Arora <viksit@mit.edu>
2016-02-05 19:02:01 +0530

    adds encode symbols behavior

--- lib/BEncoder.rb
+++ lib/BEncoder.rb
@@ -3,10 +3,13 @@ require 'BEncoder/version'
 module BEncoder
 
   class BEncoder
+
     def encode obj
       case obj
       when String
         encode_string obj
+      when Symbol
+        encode_symbol obj
       end
     end
 
@@ -15,6 +18,11 @@ module BEncoder
       def encode_string str
         "#{ str.length }:#{ str }"
       end
+
+      def encode_symbol symb
+        encode_string symb.to_s
+      end
+
   end
   
 end
--- spec/BEncoder_spec.rb
+++ spec/BEncoder_spec.rb
@@ -4,9 +4,11 @@ describe BEncoder do
 
   let(:be) { BEncoder::BEncoder.new }
 
+
   it 'has a version number' do
     # expect(BEncoder::VERSION).not_to be nil
   end
+
 
   context 'strings:' do
     it 'encodes strings correctly' do
@@ -18,4 +20,11 @@ describe BEncoder do
     end
   end
   
+
+  context 'symbols:' do
+    it 'encodes symbols correctly' do
+      expect(be.encode(:symbol)).to eq('6:symbol')
+    end
+  end
+
 end
